96c92804525ba3da3c384b45a955eb1faa256003
Polo94 <paul.henry.claudant@gmail.com>
2018-02-16 04:59:42 +0100

    modif test heroku7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
 
-  post '/', to 'home#tweet'
+  resources :home
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
